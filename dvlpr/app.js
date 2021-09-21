e39435b156c7bffd5b7789c51bb2941d75966907
Reinel Parrado <reinelparrado@gmail.com>
2021-09-21 15:05:59 -0500

    organized auth routers

--- dvlpr/app.js
+++ dvlpr/app.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const AppRouter = require('./routes/AppRouter')
-const AuthRouter = require('./routes/AuthRouter')
 
 const PORT = process.env.PORT || 3001
 
@@ -13,6 +12,5 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
-app.use('/auth', AuthRouter)
 app.use('/api', AppRouter)
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
--- dvlpr/routes/AppRouter.js
+++ dvlpr/routes/AppRouter.js
@@ -2,6 +2,10 @@ const Router = require('express').Router()
 const UserRouter = require('./UserRouter')
 const PostRouter = require('./PostRouter')
 const CommentRouter = require('./CommentRouter')
+const controller = require('../controllers/AuthController')
+
+Router.post('/auth/login', controller.Login)
+Router.post('/auth/register', controller.Register)
 
 Router.use('/users', UserRouter)
 Router.use('/posts', PostRouter)
