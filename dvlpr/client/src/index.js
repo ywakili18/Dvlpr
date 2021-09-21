ceff0c98a26feb038d1741a70a6b88dcc48a566b
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-09-21 14:21:14 -0500

    added provider to index

--- dvlpr/client/src/index.js
+++ dvlpr/client/src/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './App'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
