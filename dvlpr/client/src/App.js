da8db1cb566043d59a9d5a8968b06ddd3b8692d4
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-09-22 16:18:37 -0500

    protected route in place for /home

--- dvlpr/client/src/App.js
+++ dvlpr/client/src/App.js
@@ -4,12 +4,26 @@ import NavBar from './components/NavBar'
 import Register from './pages/Register'
 import LogIn from './pages/LogIn'
 import Feed from './pages/Feed'
+import ProtectedRoute from './components/ProtectedRoute'
+import { connect } from 'react-redux'
 
-function App() {
+const mapStateToProps = (state) => {
+  return {
+    userState: state.userState
+  }
+}
+
+function App(props) {
   return (
     <div className="App">
       <NavBar />
       <Switch>
+        <ProtectedRoute
+          authenticated={props.userState.authenticated}
+          user={props.userState.user}
+          path="/home"
+          component={Feed}
+        />
         <Route exact path="/home" component={Feed} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/" component={LogIn} />
@@ -18,4 +32,4 @@ function App() {
   )
 }
 
-export default App
+export default connect(mapStateToProps, null)(App)
