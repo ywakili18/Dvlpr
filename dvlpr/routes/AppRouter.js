18a6092caa38cc9158070b6770b108456d86fa5e
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-09-22 15:46:22 -0500

    post, newpost  work added session check

--- dvlpr/routes/AppRouter.js
+++ dvlpr/routes/AppRouter.js
@@ -1,12 +1,10 @@
 const Router = require('express').Router()
+const AuthRouter = require('./AuthRouter')
 const UserRouter = require('./UserRouter')
 const PostRouter = require('./PostRouter')
 const CommentRouter = require('./CommentRouter')
-const controller = require('../controllers/AuthController')
 
-Router.post('/auth/login', controller.Login)
-Router.post('/auth/register', controller.Register)
-
+Router.use('/auth', AuthRouter)
 Router.use('/users', UserRouter)
 Router.use('/posts', PostRouter)
 Router.use('/comments', CommentRouter)
