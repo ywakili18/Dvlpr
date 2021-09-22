586eeb11d9c7aa9d335167fc968116473a7e71c9
Reinel Parrado <reinelparrado@gmail.com>
2021-09-21 20:43:56 -0500

    fixed Routes

--- dvlpr/client/src/App.js
+++ dvlpr/client/src/App.js
@@ -1,5 +1,5 @@
 import './styles/App.css'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Register from './pages/Register'
 import LogIn from './pages/LogIn'
@@ -9,9 +9,11 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <Route path="/" component={Feed} />
-      <Route path="/register" component={Register} />
-      <Route path="/login" component={LogIn} />
+      <Switch>
+        <Route exact path="/" component={Feed} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={LogIn} />
+      </Switch>
     </div>
   )
 }
