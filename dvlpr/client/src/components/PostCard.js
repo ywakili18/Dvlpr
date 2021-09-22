1adb6a9966d1437281d83c09fcbae8ce3d198532
Reinel Parrado <reinelparrado@gmail.com>
2021-09-22 15:48:25 -0500

    merge conflict

--- dvlpr/client/src/components/PostCard.js
+++ dvlpr/client/src/components/PostCard.js
@@ -2,16 +2,11 @@ import NewPost from './NewPost'
 import NewComment from './NewComment'
 export default function PostCard() {
   return (
-<<<<<<< HEAD
-    <div className="flex flex-col justify-center items-center">
-      <div className="box-content h-32 w-32 p-4 border-4">Hello</div>
-=======
     <div>
       <div>
         <NewPost />
         <NewComment />
       </div>
->>>>>>> 2372e9bfd5c03bce146dcc5809a1113ad602dc92
     </div>
   )
 }
