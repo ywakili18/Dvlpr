8736924bf8f086a072a174024f33d1f42ebab82c
Reinel Parrado <reinelparrado@gmail.com>
2021-09-22 12:05:15 -0500

    experimental postcard

--- dvlpr/client/src/components/PostCard.js
+++ dvlpr/client/src/components/PostCard.js
@@ -1,7 +1,7 @@
 export default function PostCard() {
   return (
-    <div>
-      <div></div>
+    <div className="flex flex-col justify-center items-center">
+      <div className="box-content h-32 w-32 p-4 border-4">Hello</div>
     </div>
   )
 }
