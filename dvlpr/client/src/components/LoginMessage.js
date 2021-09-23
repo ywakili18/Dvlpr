c63a8a3e55a9c85e0c43e90a606af029d5ced709
Yousof Wakili <ywakili18@gmail.com>
2021-09-23 12:15:33 -0700

    fixed positining in nav bar

--- dvlpr/client/src/components/LoginMessage.js
+++ dvlpr/client/src/components/LoginMessage.js
@@ -1,8 +1,8 @@
 const LoginMessage = () => {
   return (
     <div>
-      <h1 class="text-black-500 text-2xl py-10  font-brand">
-        Log in to your DVLPR account!
+      <h1 class="text-purple-400 text-white text-3xl font-bold py-10 mt-20 font-brand">
+        Log in to your DVLPR account
       </h1>
     </div>
   )
