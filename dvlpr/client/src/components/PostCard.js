7964c5477498243032c85df9bc7c7e6e954d38e5
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-09-23 12:58:58 -0500

    regroup

--- dvlpr/client/src/components/PostCard.js
+++ dvlpr/client/src/components/PostCard.js
@@ -3,12 +3,48 @@ import Comments from './Comments'
 import NewComment from './NewComment'
 
 const PostCard = (props) => {
-  const [edit, toggleEdit] = useState(false)
+  const [edit, toggleEdit] = useState(true)
+  const [postContent, setPostContent] = useState(props.content)
+
+  const handleChange = (e) => {
+    setPostContent(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    toggleEdit(false)
+  }
 
   return (
     <div>
-      <div>{props.content}</div>
+      {edit ? (
+        <div>
+          <form onSubmit={handleSubmit}>
+            <textarea
+              maxLength="255"
+              name="postContent"
+              type="text"
+              value={postContent}
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+      ) : (
+        <div>{props.content}</div>
+      )}
       <div>timestamp {props.timeStamp}</div>
+      {edit ? (
+        <button
+          type="button"
+          onClick={() => {
+            toggleEdit(true)
+          }}
+        >
+          Save Edit
+        </button>
+      ) : (
+        <button type="submit">Edit</button>
+      )}
       <div>
         <div>
           {props.comments.map((comment) => (
