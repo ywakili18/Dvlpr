b3a46b04e54025aba1703d3979d0fa9a804cb178
Yousof Wakili <ywakili18@gmail.com>
2021-09-24 12:38:18 -0700

    project

---
+++ app.js
@@ -0,0 +1,23 @@
+// MAIN SERVER ROUTE
+const app = require('express')()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+
+const AppRouter = require('./routes/AppRouter')
+
+const PORT = process.env.PORT || 3001
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/api', AppRouter)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
+
+app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
