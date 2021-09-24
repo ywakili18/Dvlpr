d48af9ed41e943042e2dba71ce40c0ccd48405cb
Yousof Wakili <ywakili18@gmail.com>
2021-09-23 20:28:26 -0700

    NewComment has correct color for textarea

--- dvlpr/client/src/components/NewComment.js
+++ dvlpr/client/src/components/NewComment.js
@@ -2,33 +2,49 @@ import { useEffect } from 'react'
 
 const NewComment = () => {
   return (
-    <div class="sm:mx-auto sm:w-full sm:max-w-md text-center">
+    <div class="sm:mx-auto sm:w-full sm:max-w-md text-center text-sm">
       <div>
         <form>
           <div>
-            <div class="mt-10">
+            <div class="mt-10 flex items-center sm:mx-auto">
               <textarea
                 maxLength="255"
                 name="postContent"
                 type="text"
-                placeholder="what are your thoughts, fellow DVLPR?"
+                placeholder="what are your thoughts?"
                 class="
-                text-center 
+                w-1/2
+                text-gray-900
+                sm:px-2
+                sm:py-2
+                sm:mr-4
+                sm:ml-24
+                focus:outline-none 
                 border 
-                border-purple-400 
-                py-8 
-                px-4
-                rounded-lg shadow-sm focus:outline-none 
-                focus:ring-1 
-                md:w-full 
-                text-s"
+                border-t-2
+                border-l-2
+                border-r-2
+                border-b-2
+                rounded-lg   
+                bg-white 
+                text-center"
               />
-            </div>
 
-            <div class="mt-1">
               <button
                 type="submit"
-                class="bg-purple-regular  hover:bg-pink-hot text-coolGray-light font-bold py-3 px-5 rounded-full"
+                class="
+                h-8
+                px-4 
+                text-indigo-100 
+                transition-colors 
+                duration-150 
+                bg-gray-400 
+                rounded-lg 
+                focus:shadow-outline 
+                hover:bg-purple-800 
+                transition duration-500
+                sm:mx-auto
+                "
               >
                 +
               </button>
